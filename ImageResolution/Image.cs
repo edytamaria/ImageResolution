4ef720e7a3c07cbe4853076f1e713957e409ad9d
edytamaria <bandyta.edyta@gmail.com>
2018-01-23 00:44:22 +0100

    Copy jpg to folder
    Copy images from one directory to another, set new name if the same file exists

---
+++ ImageResolution/Image.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ImageResolution
+{
+    class Image
+    {
+        public Image(){}
+        
+        public void NewResolution(string file, string outputDir, int[] resolution)
+        {
+            string fname = FindFileName(file);            
+           
+            using (Bitmap bitmap = (Bitmap)System.Drawing.Image.FromFile( file, true))
+            {
+                using (Bitmap newBitmap = new Bitmap(bitmap))
+                {
+                    int counter = 1;
+                    newBitmap.SetResolution(resolution[0], resolution[1]);
+                    // --------- check if the same name exists --------------
+                    while(File.Exists(outputDir + "\\" + fname + counter + ".jpg"))
+                    {
+                        var cooo = outputDir + "\\" + fname + counter + ".jpg";
+                        counter++;
+                    }
+                    var newdirectory = outputDir + "\\" + fname + counter + ".jpg";
+                    newBitmap.Save(newdirectory, ImageFormat.Jpeg);
+                }
+            }
+            Console.WriteLine("Opracja udana.");
+        }
+
+        public void CreateImagesWithNewResolution(string inputDir, string outputDir, int[] resolution)
+        {
+            var files = Directory.GetFiles(inputDir, "*", SearchOption.TopDirectoryOnly);
+            int fCount = files.Length;
+            foreach (string file in Directory.EnumerateFiles(inputDir, "*.jpg"))
+            {
+                NewResolution(file, outputDir, resolution);
+            }
+        }
+
+        public string FindFileName(string file)
+        {
+            var findname = file.Split('\\');
+            string fname = findname[findname.Length - 1].Split('.')[0];
+            return fname;
+        }
+    }
+}
--- ImageResolution/Program.cs
+++ ImageResolution/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace ImageResolution
 {
@@ -10,6 +15,48 @@ namespace ImageResolution
     {
         static void Main(string[] args)
         {
+           
+            Console.WriteLine("Podaj ścieżkę do katalogu: ");
+            string _inputDir = Console.ReadLine();
+            if (!Directory.Exists(_inputDir))
+            {
+                _inputDir = Directory.GetCurrentDirectory();
+                Console.WriteLine("niepoprawna ścieżka");
+            }
+            else
+                Console.WriteLine("poprawna ścieżka");
+
+            Console.WriteLine("Podaj docelową ścieżkę do katalogu: ");
+            string _outputDir = Console.ReadLine();
+            if (!Directory.Exists(_outputDir))
+            {
+                _outputDir = _inputDir + "\\copy";
+                int counter = 1;
+                while (Directory.Exists(_outputDir))
+                {
+                    _outputDir = _outputDir + counter;
+                    counter++;
+                }
+                Directory.CreateDirectory(_outputDir);
+            }
+            else
+                Console.WriteLine("poprawny");
+
+            Console.WriteLine("Podaj nową rozdzielczość: ");
+            string res = Console.ReadLine();
+
+            //string resPattern = "([0-9])\\w([0-9])";
+            int[] newResolution = new int[2];
+            newResolution[0] =Int32.Parse( res.Split(' ')[0]);
+            newResolution[1] = Int32.Parse(res.Split(' ')[1]);
+
+
+            Image image = new Image();
+            image.CreateImagesWithNewResolution(_inputDir, _outputDir, newResolution);
+
+            Console.ReadKey();
+            
+
         }
     }
 }
